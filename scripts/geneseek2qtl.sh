fe076d35de3f784eb871d1c838800074d687e946
Sam Widmayer <samuel.widmayer@jax.org>
2022-11-18 09:58:15 -0500

    Delete geneseek2qtl.sh

--- scripts/geneseek2qtl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -J GS2QTL2
-#SBATCH --mem 100GB
-#SBATCH -t 8:00:00
-
-# Performing Marker QC on GigaMUGA samples
-singularity run docker://sjwidmay/do4wc_hr:latest scripts/geneseek2qtl2.R

11dadd4d50f6c8e3f30b90c8842150cf80cc3423
sam-widmayer <samuel.widmayer@jax.org>
2022-12-14 11:09:28 -0500

    run container on test

--- scripts/test_sam2bam.sh
+++ scripts/test_sam2bam.sh
@@ -1,1 +1,9 @@
-samtools view -S -b test.sam > test.bam
+#!/bin/bash
+#SBATCH -J CC_sam2bam_test
+#SBATCH --mem 100GB
+#SBATCH -t 24:00:00
+
+fastq_dir=/fastscratch/widmas
+cd ${fastq_dir}
+
+singularity run /projects/omics_share/meta/containers/quay.io-biocontainers-samtools-1.14--hb421002_0.img samtools view -S -b test.sam > test.bam
